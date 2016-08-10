2f721b4fd500798bf514dfabea0326153cbd3eb6
Antoni Boucher <bouanto@zoho.com>
2016-08-09 21:25:49 -0400

    Fixed example.

--- src/lib.rs
+++ src/lib.rs
@@ -28,17 +28,15 @@
 //! ```
 //! extern crate des;
 //!
-//! use des::{decrypt, encrypt};
-//!
 //! let key = [0x13, 0x34, 0x57, 0x79, 0x9B, 0xBC, 0xDF, 0xF1];
 //! let message = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF];
-//! let cipher = encrypt(&message, &key);
-//! let message = decrypt(&cipher, &key);
+//! let cipher = des::encrypt(&message, &key);
+//! let message = des::decrypt(&cipher, &key);
 //! ```
 //!
 //! # Usage
 //!
-//! Des exports two functions: encrypt and decrypt.
+//! Des exports two functions: `encrypt` and `decrypt`.
 //! Use the former to encrypt some data with a key and the later to decrypt the data.
 
 /*
